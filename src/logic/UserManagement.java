4ade59c35a3b141b22b5422c7fd95ef672e43a92
Marco <pimientapfoehler@gmail.com>
2023-08-28 16:04:21 +0200

    Added UserManagement

--- src/logic/UserManagement.java
+++ src/logic/UserManagement.java
@@ -1,4 +1,27 @@
 package logic;
 
+import dao.User;
+import database.InMemoryDatabase;
+
 public class UserManagement {
+
+    public InMemoryDatabase database;
+
+    public UserManagement() {
+        this.database = new InMemoryDatabase();
+    }
+
+    public boolean saveUser(User user) {
+        return this.database.addUser(user);
+    }
+
+    public User authentificateUser(String email, String password){
+        User user = this.database.getUser(email);
+        if (user != null) {
+            if (user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
